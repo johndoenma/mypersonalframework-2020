766522f4a1e1622e70836e32da7f87ba91b3ae1e
kccnma <gargiulo@hawaii.edu>
2020-10-28 13:19:17 -1000

    added custom bespoke scroll-triggered animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 // OVERLAY NAV MENU SHOW HIDE
-
 const mymenubutton = document.querySelector('.menu-button');
 const mysitenav = document.querySelector('.site-header .site-nav');
 
@@ -11,3 +10,18 @@ mymenubutton.onclick = function () {
         mysitenav.setAttribute('data-navstate', 'open')
     }
 };
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+    myobserver.observe(section);
+});
